0595869057fca8ff893531166742fe5271920b24
Ambrose Adeya Ahawo <ahawoadeya995@gmail.com>
2021-09-23 11:05:22 +0300

    new commit

---
+++ src/1. useState/1. counter/Counter.js
@@ -0,0 +1,37 @@
+import React, { useState } from 'react'
+import './Counter.css'
+
+const Counter = () => {
+    const [value, setValue] = useState(0)
+
+    const handleIncrease = () => {
+        setValue(value + 1)
+    }
+
+    const handleDecrease = () => {
+        if (value < 1) {
+            setValue(0)
+        }else {
+            setValue(value - 1)
+        }
+    }
+
+    const handleReset = () => {
+        setValue(0)
+    }
+    return (
+        <div className="container">
+            <h2>React State Example 1 - Counter</h2>
+            <div className="inner-content">
+                <h1>{value}</h1>
+                <div className="buttons">
+                    <button onClick={handleIncrease} >increase</button>
+                    <button onClick={handleDecrease} >Decrease</button>
+                    <button onClick={handleReset} >Reset</button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Counter
